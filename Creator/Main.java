bd6152ab287b7b00ca65821572b344ef27470060
Joseph <jaathunder@yahoo.com>
2019-10-23 14:40:44 -0700

    Add member class, ability to add naming and other attributes to them

--- Creator/Main.java
+++ Creator/Main.java
@@ -1,10 +1,70 @@
 package Creator;
 
+import java.util.Scanner;
+//import Creator.Member; //only use if class is in a different package
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("===Family Tree Creator===");
+        System.out.println("Enter name of family: ");
+
+        Scanner kb = new Scanner(System.in);
+        String familyName = kb.next();
+
+        System.out.println("Your family name is: " + familyName);
+
+        try {
+            System.out.println("Add a new family member? 1 = yes; 0 = no");
+            int choice = kb.nextInt();
+
+            if (choice == 0) {
+                System.out.println("exiting!");
+            }else if (choice == 1) {
+                createMember();
+            }else{
+                System.out.println("invalid number.");
+            }
+
+        } catch(Exception e) {
+            System.out.println("Invalid input.");
+        }
+
+        //class cannot be accessed from here, need to find a way to do that later
+
+        //TODO: add checking for numbers, must only be chars
+        //TODO: add saving for family tree on a file, also to be retrieved
+
+
+    }
+
+    public static void createMember() {
+        Member m = new Member();
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print("enter first name: ");
+        try {
+            m.addFirstName(kb.next());
+        }catch (Exception e) {
+            System.out.println("Invalid input.");
+        }
+
+        System.out.print("enter middle name, leave blank if there is no middle name: ");
+        try {
+            m.addMiddleName(kb.next());
+        }catch (Exception e) {
+            System.out.println("Invalid input.");
+        }
+
+        System.out.print("enter last name: ");
+        try {
+            m.addLastName(kb.next());
+        }catch (Exception e) {
+            System.out.println("Invalid input.");
+        }
+
+        System.out.println("This member's name is: " + m.getFirstName() + " " + m.getMiddleName() + " " + m.getLastName() + ".");
 
     }
 
---
+++ Creator/Member.java
@@ -0,0 +1,59 @@
+package Creator;
+
+public class Member {
+
+    //family member attributes
+    //will use a constructor when making family members
+    private String firstName;
+    private String middleName; //null will have to be handled if skipping a middle name
+    //this can have multiple middle names with a space in between
+    private String lastName;
+
+    private int gender; //0 = female, 1 = male
+    private String relation; //how the member is related in the family
+
+    //add information into the member
+    public void addFirstName (String name) {
+        firstName = name;
+    }
+    public void addMiddleName (String name) {
+        middleName = name;
+    }
+    public void addMiddleName () {
+        middleName = "none";
+    }
+    public void addLastName (String name) {
+        lastName = name;
+    }
+    public void setGender (int g) {
+        gender = g;
+    }
+    public void setRelation (String r) {
+        relation = r;
+    }
+
+    //retrieve information about the member
+    public String getFirstName () {
+        return firstName;
+    }
+    public String getMiddleName () {
+        if (middleName == null) {
+            return "no middle name";
+        }
+        return middleName;
+    }
+    public String getLastName () {
+        return lastName;
+    }
+    public String getGender () {
+        if (gender == 0) {
+            return "female";
+        }else{
+            return "male";
+        }
+    }
+    public String getRelation () {
+        return relation;
+    }
+
+}
